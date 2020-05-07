c4ca4fa47705bf4dde33bce26f0c21b8b30d76f3
Andrés Parrado <andresparrado@Andress-MBP.home>
2020-05-06 22:38:52 -0400

    Finalized P6.py

--- homeworks/HW2/HW2-final/P6.py
+++ homeworks/HW2/HW2-final/P6.py
@@ -6,7 +6,19 @@ Created on Sun May  3 16:27:16 2020
 @author: andresparrado
 """
 import math
-#import numpy as np
+import numpy as np
+import time
+
+# Decorator
+def timer(f):
+    def inner(*args):
+        t0 = time.time()
+        output = f(*args)
+        elapsed = time.time() - t0
+        print("Time Elapsed", elapsed)
+        return output
+    return inner
+
 
 # Importing the file
 file_name = "circles.txt"
@@ -19,8 +31,11 @@ def area_circle(radius):
     area = math.pi*(radius**2)
     return area
 
-areas = [0.0]*len(radii)
+
 # Defining the second function
+areas = [0.0]*len(radii)
+
+@timer
 def my_avg(radii_list, area_circle):
     for i in range(len(radii_list)):
         area = area_circle(radii_list[i])
@@ -29,5 +44,19 @@ def my_avg(radii_list, area_circle):
     
     print("My average was {}".format(final_average))
 
-# Using the second function    
+
+# Moving on to the numpy function
+areas_np = np.zeros(len(radii))
+
+@timer
+def np_ave(radii_list, area_circle):
+    for i in range(len(radii_list)):
+        area = area_circle(radii_list[i])
+        areas[i]=area
+    final_average = sum(areas)/len(areas)
+    print("My average was {}".format(final_average))
+
+
+# Using the second function     
 my_avg(radii,area_circle)
+np_ave(radii,area_circle)
