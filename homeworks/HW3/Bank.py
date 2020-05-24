223346b2e32726abc061360a09c0e6d7f34c08ff
Andrés Parrado <andresparrado@Andress-MBP.home>
2020-05-24 11:10:03 -0400

    finished basic functionality of Bank module and created P3B_tests. Need to add the tests and create closures

--- homeworks/HW3/Bank.py
+++ homeworks/HW3/Bank.py
@@ -20,25 +20,93 @@ class NotEnoughMoney(Exception):
 class NegativeAmount(Exception):
     pass
 
+class AccountError(Exception):
+    pass
 
 
 
-
+# BankAccount class
 class BankAccount():
+    ''' This class creates a bank account. The main
+        methods are withdraw and deposit. You cannot
+        withdraw more than your balance. Also, you
+        cannot withdraw or deposit a negative amount.
+    '''
     
     def __init__(self, owner, accountType): 
         self.owner = owner
         self.accountType = accountType
         self.balance = 0
         
+        
     def withdraw(self, amount):
         if self.balance - amount < 0:
-            raise NotEnoughMoney('Cannot withdraw more than balance')
-            
+            raise NotEnoughMoney('Cannot withdraw more than balance') 
         elif amount < 0:
             raise NegativeAmount('Cannot withdraw negative amount')
-        
         else:
             self.balance -= amount
+    
+    
+    def deposit(self, amount):
+        if amount < 0:
+            raise NegativeAmount('Cannot withdraw negative amount')
+        else:
+            self.balance += amount
+    
+    
+    def __str__(self):
+        return 'a Savings'
+    
+    
+    def __len__(self):
+        return self.balance
+    
 
-testaccount = BankAccount("Andrés", AccountType.SAVINGS)
+
+# BankUser class
+class BankUser():
+    def __init__(self,owner):
+        self.owner = owner
+        self.checking = None
+        self.savings = None
+    
+    
+    def addAccount(self,accountType):
+        if accountType == AccountType.SAVINGS:
+            if self.savings != None:
+                raise AccountError('Bank account of this type already present')
+            self.savings = BankAccount(self.owner,accountType)
+
+        elif accountType == AccountType.CHECKING:
+            if self.checking != None:
+                raise AccountError('Bank account of this type already present')
+            self.checking  = BankAccount(self.owner,accountType)
+            
+            
+    def getBalance(self, accountType):
+        if accountType == AccountType.SAVINGS:
+            return self.savings.__len__()
+        if accountType == AccountType.CHECKING:
+            return self.checking.__len__()
+        
+        
+    def deposit(self, accountType, amount):
+        if accountType == AccountType.SAVINGS:
+            return self.savings.deposit(amount)
+        if accountType == AccountType.CHECKING:
+            return self.checking.deposit(amount)
+         
+            
+    def withdraw(self, accountType, amount):
+        if accountType == AccountType.SAVINGS:
+            return self.savings.withdraw(amount)
+        if accountType == AccountType.CHECKING:
+            return self.checking.withdraw(amount)
+    
+    def __str__(self):
+        return 'User currently has {} and {} accounts'.format(self.checking,self.savings)
+         
+            
+
+
